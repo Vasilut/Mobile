2019636375aff7e0477e38c9950f7ad31a7d816a
Vasilut Lucian <lucian.vasilut10@gmail.com>
2018-03-23 16:34:32 +0200

    Added list view project.

---
+++ ListViews/ListViews/ListViews/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,22 @@
+﻿using ListViews.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace ListViews.ViewModel
+{
+    public class MainPageViewModel
+    {
+        public ObservableCollection<Person> People { get; set; } = new ObservableCollection<Person>();
+
+        public MainPageViewModel()
+        {
+            for(int i = 0;  i < 5; ++i)
+            {
+                Person p = new Person { Name = $"Luci {i}", Address = $"AdressOras {i + 1}" };
+                People.Add(p);
+            }
+        }
+    }
+}
