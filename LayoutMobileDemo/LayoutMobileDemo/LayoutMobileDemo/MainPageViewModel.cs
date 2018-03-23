1ad4d012bcf084d4ee53eaa941b0656801a42c89
Vasilut Lucian <lucian.vasilut10@gmail.com>
2018-03-23 14:11:08 +0200

    Added binding and command.

--- LayoutMobileDemo/LayoutMobileDemo/LayoutMobileDemo/MainPageViewModel.cs
+++ LayoutMobileDemo/LayoutMobileDemo/LayoutMobileDemo/MainPageViewModel.cs
@@ -1,13 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
+using Xamarin.Forms;
 
 namespace LayoutMobileDemo
 {
-    public class MainPageViewModel
+    public class MainPageViewModel : INotifyPropertyChanged
     {
         public string Prompt { get; set; }
         public string Name { get; set; }
+
+        private string _labelText = "Hello";
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged([CallerMemberName] string caller = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(caller));
+            }
+        }
+
+        public string LabelText
+        {
+            get { return _labelText; }
+            set
+            {
+                _labelText = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private Command _changeTextCommand;
+
+        public Command ChangeTextCommand
+        {
+            get
+            {
+                return _changeTextCommand ?? (_changeTextCommand = new Command(() => 
+                {
+
+                    LabelText = "GoodBye";
+                }));
+            }
+        }
 
         public MainPageViewModel()
         {
