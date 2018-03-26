0ed6cfc71650d871c9972e5e4af28f49d037f1ff
Vasilut Lucian <lucian.vasilut10@gmail.com>
2018-03-26 15:30:21 +0300

    Added mesages.

---
+++ Message/Message/Message/MainPageViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Message
+{
+    public class MainPageViewModel
+    {
+        public Command ButtonClickedCommand { get; set; }
+
+        public MainPageViewModel()
+        {
+            ButtonClickedCommand = new Command(HandleButtonClick);
+        }
+
+        private void HandleButtonClick()
+        {
+            MessagingCenter.Send<MainPageViewModel>(this, "ButtonClicked");
+        }
+    }
+}
