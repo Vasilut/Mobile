896a3494f2dc3651995e2a94d5bb7780fd644b04
Vasilut Lucian <lucian.vasilut10@gmail.com>
2018-03-26 11:41:21 +0300

    Added convertor resources.

--- ListViews/ListViews/ListViews/Model/Person.cs
+++ ListViews/ListViews/ListViews/Model/Person.cs
@@ -9,6 +9,7 @@ namespace ListViews.Model
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Address { get; set; }
-        public string ImageSource { get; set; }
+        public string ImageSource { get; set; } 
+        public decimal Age { get; set; }
     }
 }
--- ListViews/ListViews/ListViews/ViewModel/MainPageViewModel.cs
+++ ListViews/ListViews/ListViews/ViewModel/MainPageViewModel.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
+using static System.Math;
 
 namespace ListViews.ViewModel
 {
@@ -12,9 +13,11 @@ namespace ListViews.ViewModel
 
         public MainPageViewModel()
         {
+            Random rand = new Random();
             for(int i = 0;  i < 5; ++i)
             {
                 Person p = new Person { FirstName = $"Luci {i}", LastName= $"Vasilut{i}", Address = $"AdressOras {i + 1}" , ImageSource = $"man{i}.jpg"};
+                p.Age = (decimal)(35 + rand.NextDouble());
                 People.Add(p);
             }
         }
