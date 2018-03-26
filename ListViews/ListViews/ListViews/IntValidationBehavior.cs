66a5040d0c479f1c076df36cc9216d6feaca8134
Vasilut Lucian <lucian.vasilut10@gmail.com>
2018-03-26 13:06:29 +0300

    Added behaviors.

---
+++ ListViews/ListViews/ListViews/IntValidationBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ListViews
+{
+    public class IntValidationBehavior: Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            bindable.TextChanged += OnEntryTextChanged;
+            base.OnAttachedTo(bindable);
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            bindable.TextChanged -= OnEntryTextChanged;
+            base.OnDetachingFrom(bindable);
+        }
+
+
+        private void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+        {
+            bool valid = int.TryParse(e.NewTextValue, out int result);
+            Entry entry = sender as Entry;
+            entry.TextColor = valid ? Color.Default : Color.Red;
+        }
+    }
+}
